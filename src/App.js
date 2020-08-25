04d0091ed64f23030530e6f827b88c1899106d1e
buscaroli <m.buscaroli@outlook.com>
2020-08-25 17:44:03 +0100

    Added the Search component to create a list of related wikipedia articles with a link button to open the related wikipedia page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordion from './components/Accordion'
+import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const items = [
+    { title: 'Title ONE of the Accordion',
+      content: 'Stet ea kasd nonumy ut consetetur amet takimata ea, amet dolores sed sed accusam sadipscing elitr, tempor accusam est sea dolor elitr dolor diam.'
+    },
+    { title: 'Title TWO of the Accordion',
+      content: 'Aliquyam et ipsum amet at magna vero stet sanctus sanctus. Dolor eos.'
+    },
+    { title: 'Title THREE of the Accordion', 
+      content: 'Sanctus sea lorem lorem ipsum eos amet takimata sea. Sit labore at labore gubergren ut, dolore.'
+    }
+    ]
+
+export default () => {
+    return(
+        <Search />
+    )
 }
-
-export default App;
