8318a51437ce122f8490bc94031ac92614e8cfdc
buscaroli <m.buscaroli@outlook.com>
2020-08-30 16:02:48 +0100

    Implementing a very basic routing feature, avoiding the use of react-router on purpose.

--- src/App.js
+++ src/App.js
@@ -5,38 +5,66 @@ import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
 
 const items = [
-    { title: 'Title ONE of the Accordion',
-      content: 'Stet ea kasd nonumy ut consetetur amet takimata ea, amet dolores sed sed accusam sadipscing elitr, tempor accusam est sea dolor elitr dolor diam.'
-    },
-    { title: 'Title TWO of the Accordion',
-      content: 'Aliquyam et ipsum amet at magna vero stet sanctus sanctus. Dolor eos.'
-    },
-    { title: 'Title THREE of the Accordion', 
-      content: 'Sanctus sea lorem lorem ipsum eos amet takimata sea. Sit labore at labore gubergren ut, dolore.'
-    }
-    ]
+  { title: 'Title ONE of the Accordion',
+    content: 'Stet ea kasd nonumy ut consetetur amet takimata ea, amet dolores sed sed accusam sadipscing elitr, tempor accusam est sea dolor elitr dolor diam.'
+  },
+  { title: 'Title TWO of the Accordion',
+    content: 'Aliquyam et ipsum amet at magna vero stet sanctus sanctus. Dolor eos.'
+  },
+  { title: 'Title THREE of the Accordion', 
+    content: 'Sanctus sea lorem lorem ipsum eos amet takimata sea. Sit labore at labore gubergren ut, dolore.'
+  }
+]
 
-    const options = [
-      {
-       label: 'Red',
-       color: 'red'
-    },
-    {
-      label: 'Green',
-      color: 'green'
-    },
-    {
-      label: 'Blue',
-      color: 'blue'
-    }
-    ]
+const options = [
+  {
+    label: 'Red',
+    color: 'red'
+  },
+  {
+    label: 'Green',
+    color: 'green'
+  },
+  {
+    label: 'Blue',
+    color: 'blue'
+  }
+]
+
+
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />
+  } 
+}
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />
+  }
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />
+  }
+}
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />
+  }
+}
+
 
 export default () => {
-  
-    return(
-      <div>
-        <Translate />
-      </div>
-       
-    )
+  return(
+    <div>
+      {showAccordion()} 
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
+    </div>
+      
+  )
 }
